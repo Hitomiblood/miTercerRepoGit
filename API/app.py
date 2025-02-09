bb03ac2fe502a5d7c99809a281b7110e5463af0f
Miguel Santiago Gómez Suárez <Hitomiblood@gmail.com>
2025-02-09 09:54:52 -0500

    Se realiza creación denueva tarea

--- API/app.py
+++ API/app.py
@@ -10,10 +10,16 @@ def get_familia():
     rows = ["Amin", "Marce", "Miranda"]
     return rows
 
+
 @app.get("/superheroesDC")
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg", "Robin"]
     return rows
 
+
+@app.get("/CursosPlatzi")
+def get_cursos():
+    rows = ["Python", "Django", "Flask", "FastAPI", "JavaScript", "React", "Vue", "Angular", "NodeJS", "MongoDB"]
+    return rows
 
 
